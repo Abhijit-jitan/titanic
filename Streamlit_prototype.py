372e47098907c1ad478c6bb43bdf0b5b827fcd38
Abhijit <55889831+Abhijit-jitan@users.noreply.github.com>
2021-02-07 02:11:50 -0500

    Update Streamlit_prototype.py

--- Streamlit_prototype.py
+++ Streamlit_prototype.py
@@ -20,18 +20,18 @@ st.write("""
 st.sidebar.header("User Input Parameters")
 
 def user_input():
-    age = st.sidebar.slider("Your Age", 1,75,30)
-    fare = st.sidebar.slider("Fare in 1912 $ (See Info)",15,500,40)
-    SibSp = st.sidebar.selectbox("How many siblings or spouses are travelling with you?",[0,1,2,3,4,5,6,7,8])
-    Parch = st.sidebar.selectbox("How many parents or children are travelling with you?",[0,1,2,3,4,5,6,7,8])
-    cabin_multiple = st.sidebar.selectbox("How many additional cabins have you booked?",[0,1,2,3,4])
+    age = st.slider("Your Age", 1,75,30)
+    fare = st.slider("Fare in 1912 $ (See Info)",15,500,40)
+    SibSp = st.selectbox("How many siblings or spouses are travelling with you?",[0,1,2,3,4,5,6,7,8])
+    Parch = st.selectbox("How many parents or children are travelling with you?",[0,1,2,3,4,5,6,7,8])
+    cabin_multiple = st.selectbox("How many additional cabins have you booked?",[0,1,2,3,4])
     numeric_ticket = rd.randint(0, 1)
-    Sex = st.sidebar.selectbox("Select your Gender (due to the historical nature of the data only male/female are available)",["male","female"])
+    Sex = st.selectbox("Select your Gender (due to the historical nature of the data only male/female are available)",["male","female"])
     Sex_female = 0 if Sex == "male" else 1
     Sex_male = 0 if Sex == "female" else 1
-    Pclass = st.sidebar.selectbox("Which Class is your ticket from?  (For accurate predictions match with Fare)", [1,2,3])
+    Pclass = st.selectbox("Which Class is your ticket from?  (For accurate predictions match with Fare)", [1,2,3])
     Pclass_1 = 1 if Pclass == 1 else 0; Pclass_2 = 1 if Pclass == 2 else 0; Pclass_3 = 1 if Pclass == 3 else 0
-    boarding = st.sidebar.selectbox("Where did you board the Titanic?", ["Cherbourg","Queenstown","Southampton"])
+    boarding = st.selectbox("Where did you board the Titanic?", ["Cherbourg","Queenstown","Southampton"])
     Embarked_C = 1 if boarding == "Cherbourg" else 0; Embarked_Q = 1 if boarding == "Queenstown" else 0; Embarked_S = 1 if boarding == "Southampton" else 0
     df = {"Age": age,"norm_fare":np.log(fare+1),"SibSp":SibSp,"Parch":Parch,"cabin_multiple": cabin_multiple,
             "numeric_ticket":numeric_ticket,"Sex_female":Sex_female,"Sex_male":Sex_male,"Pclass_1": Pclass_1, "Pclass_2": Pclass_2, "Pclass_3": Pclass_3,
